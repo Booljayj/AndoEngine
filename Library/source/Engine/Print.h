c635556e01ee1a3c39129d4e7f5ebb7d83c52dc5
Justin <booljayj@gmail.com>
2018-01-15 00:09:38 +0100

    Create template object logging function

--- Library/source/Engine/Print.h
+++ Library/source/Engine/Print.h
@@ -4,3 +4,10 @@
 
 const char* l_printf( CTX_ARG, const char* Format, ... );
 l_string l_sprintf( CTX_ARG, const char* Format, ... );
+
+//Describe an object in a way that can be logged as output or used as an input to a formatting operation.
+template< class T >
+const char* Describe( CTX_ARG, T& Obj ) { return "[UNKNOWN]"; }
+
+//A macro to simplify logging with Describe, so you don't have to always type the context argument
+#define Desc( Obj ) Describe( CTX, Obj )
