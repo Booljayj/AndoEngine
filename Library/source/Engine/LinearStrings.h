6351c0fec36e163f121c26c58f7e91792f3492b6
Justin <booljayj@gmail.com>
2018-01-14 22:25:29 +0100

    Remove linear streams that don't work as intented

--- Library/source/Engine/LinearStrings.h
+++ Library/source/Engine/LinearStrings.h
@@ -1,6 +1,5 @@
 #pragma once
 #include <string>
-#include <sstream>
 #include <cstdarg>
 #include <cstdio>
 #include "Engine/LinearAllocator.h"
@@ -12,24 +11,6 @@ using l_string = l_basic_string<char>;
 using l_wstring = l_basic_string<wchar_t>;
 using l_u16string = l_basic_string<char16_t>;
 using l_u32string = l_basic_string<char32_t>;
-
-template< class CharT >
-using l_basic_istringstream = std::basic_istringstream<CharT, std::char_traits<CharT>, TLinearAllocator<CharT>>;
-
-using l_istringstream = l_basic_istringstream<char>;
-using l_wistringstream = l_basic_istringstream<wchar_t>;
-
-template< class CharT >
-using l_basic_ostringstream = std::basic_ostringstream<CharT, std::char_traits<CharT>, TLinearAllocator<CharT>>;
-
-using l_ostringstream = l_basic_ostringstream<char>;
-using l_wostringstream = l_basic_ostringstream<wchar_t>;
-
-template< class CharT >
-using l_basic_stringstream = std::basic_stringstream<CharT, std::char_traits<CharT>, TLinearAllocator<CharT>>;
-
-using l_stringstream = l_basic_stringstream<char>;
-using l_wstringstream = l_basic_stringstream<wchar_t>;
 
 const char* l_printf( LinearAllocatorData& Allocator, const char* Format, ... )
 {
