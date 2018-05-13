02bc0a62273d47bdbb1882fbac22fc9a0e8422d2
Justin <booljayj@gmail.com>
2018-05-13 22:13:09 +0200

    Add custom string comparison function

--- Library/source/Reflection/Resolver/StringResolver.cpp
+++ Library/source/Reflection/Resolver/StringResolver.cpp
@@ -6,6 +6,10 @@ namespace Reflection {
 	StructTypeInfo TypeInfo__std_string{
 		[]( TypeInfo* Info ) {
 			Info->Description = "dynamic string";
+			Info->Compare = []( TypeInfo* Info, void const* A, void const* B ) -> int8_t {
+				return static_cast<std::string const*>( A )->compare( *static_cast<std::string const*>( B ) );
+			};
+
 			if( auto* StructInfo = Info->As<StructTypeInfo>() ) {
 				//@todo Fill out function info for std::string
 			}
