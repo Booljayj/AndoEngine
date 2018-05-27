77c8e5578709e46dc953fa3b7c307385c3eec6b2
Justin <booljayj@gmail.com>
2018-05-27 09:47:41 +0200

    Modify the string id function to work with non-null-terminated strings

--- Library/source/Engine/StringID.h
+++ Library/source/Engine/StringID.h
@@ -6,9 +6,8 @@ constexpr inline uint32_t operator ""_id( char const* Str, size_t Size )
 {
 	//DJB2 Algorithm
 	uint32_t Ret = 5381;
-	while( *Str ) {
+	for( size_t Index = 0; Index < Size; ++Index, ++Str ) {
 		Ret = ( Ret * 33 ) + *Str;
-		++Str;
 	}
 	return Ret;
 }
@@ -16,7 +15,16 @@ constexpr inline uint32_t operator ""_id( char const* Str, size_t Size )
 constexpr inline uint32_t id( char const* Str )
 {
 	if( Str != nullptr ) {
-		return operator""_id( Str, 0 );
+		return operator""_id( Str, strlen( Str ) );
+	} else {
+		return 0;
+	}
+}
+
+constexpr inline uint32_t id( std::string_view Str )
+{
+	if( Str.data() != nullptr ) {
+		return operator""_id( Str.data(), Str.length() );
 	} else {
 		return 0;
 	}
