b4694a0ba865009c610633688e314e04a43f62c6
Justin <booljayj@gmail.com>
2018-10-03 23:06:56 +0200

    Fix broken functions related to code point tracking

--- Library/source/Rendering/Font/FontComponent.h
+++ Library/source/Rendering/Font/FontComponent.h
@@ -24,11 +24,14 @@ struct CodePointFrequencyTracker {
 
 	/** Add the contents of a string to the tracked code points */
 	void TrackCodePoints( CTX_ARG, std::string_view String ) {
-		std::wstring_convert<std::codecvt_utf8<char32_t>, char32_t, TLinearAllocator<char32_t>, TLinearAllocator<char>> Converter{};
-		l_u32string CodePointString = Converter.from_bytes( String.data(), String.data() + String.length() );
-		for( char32_t CodePoint : CodePointString ) {
-			++CodePointFrequencyMap[CodePoint];
-		}
+		//@todo The standard code converting algorithms won't allow the use of the temporary allocator.
+		// This is a no-go, we need to find an alternate method
+
+		// std::wstring_convert<std::codecvt_utf8<char32_t>, char32_t, TLinearAllocator<char32_t>> Converter{};
+		// l_u32string CodePointString = Converter.from_bytes( String.data(), String.data() + String.length() );
+		// for( char32_t CodePoint : CodePointString ) {
+		// 	++CodePointFrequencyMap[CodePoint];
+		// }
 	}
 
 	/** Returns a temporary array of all code points in this set, ordered with the most frequent code points first */
@@ -58,6 +61,7 @@ struct CodePointFrequencyTracker {
 		for( size_t Index = 0; Index < Count; ++Index ) {
 			Characters[Index] = Pairs[Index].first;
 		}
+		return Characters;
 	}
 };
 
