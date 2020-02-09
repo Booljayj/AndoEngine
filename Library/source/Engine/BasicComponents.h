e4477df5f12c2ccb298ff30a4c7d3b744d5db656
Justin <booljayj@gmail.com>
2020-02-09 01:21:16 -0800

    Reformat Engine files to new code standard

--- Library/source/Engine/BasicComponents.h
+++ Library/source/Engine/BasicComponents.h
@@ -6,7 +6,7 @@
 /** Allows an entity to exist at a point in space, often relative to another entity */
 struct TransformComponent {
 	/** The position of the matrix for this component in a transform hierarchy */
-	std::tuple<size_t, size_t> TransformHierarchyID;
+	std::tuple<size_t, size_t> transformHierarchyID;
 };
 
 // see https://youtu.be/W45-fsnPhJY?list=WL&t=811 for information on this
@@ -14,26 +14,25 @@ struct TransformComponent {
 /** Contains a full hierarchy of transforms in forward-iteration order */
 struct TransformHierarchy {
 	struct HierarchyEntry {
-		bool IsDirty; //True if the local matrix has been modified and the world matrix needs to be updated
-		glm::mat4x4 LocalMatrix; //Updated whenever the object itself is changed
-		glm::mat4x4 WorldMatrix; //Updated whenever the hierarchy is re-evaluated
+		bool isDirty; //True if the local matrix has been modified and the world matrix needs to be updated
+		glm::mat4x4 localMatrix; //Updated whenever the object itself is changed
+		glm::mat4x4 worldMatrix; //Updated whenever the hierarchy is re-evaluated
 	};
 
-	bool bShouldRebuild; //If true, we need to recreate the array of matrices because the hierarchy has changed. This is slow!
-	bool bIsDirty; //If true, we need to iterate through the matrices and recalculate the dirty ones
-	TransformComponent* Root; //The transform at the root of the hierarchy (it does not have a parent)
-	std::vector<HierarchyEntry> Entries; //Entries in forward-iteration order starting with the root, parents will always appear before their children */
+	bool shouldRebuild; //If true, we need to recreate the array of matrices because the hierarchy has changed. This is slow!
+	bool isDirty; //If true, we need to iterate through the matrices and recalculate the dirty ones
+	TransformComponent* root; //The transform at the root of the hierarchy (it does not have a parent)
+	std::vector<HierarchyEntry> entries; //entries in forward-iteration order starting with the root, parents will always appear before their children */
 };
 
 using TransformComponentManager = TSimpleComponentManager<TransformComponent>;
 
 /** Allows an entity to form a heirarchy with other entities */
-struct HierarchyComponent
-{
-	HierarchyComponent* Parent;
-	std::vector<HierarchyComponent*> Children;
+struct HierarchyComponent {
+	HierarchyComponent* parent;
+	std::vector<HierarchyComponent*> children;
 
-	//Event<HierarchyComponent*, HierarchyComponent*> HierarchyChanged;
+	//Event<HierarchyComponent*, HierarchyComponent*> hierarchyChanged;
 };
 
 using HierarchyComponentManager = TSimpleComponentManager<HierarchyComponent>;
