299c037722a87cc7554f89c970dffc1faf900136
Justin <booljayj@gmail.com>
2020-02-16 17:46:28 -0800

    Fix member names in the ScopedTempBlock macros

--- Library/source/Engine/ScopedTempBlock.h
+++ Library/source/Engine/ScopedTempBlock.h
@@ -14,6 +14,6 @@ private:
 };
 
 /** Used at the beginning of a scope to indicate that temporary allocations will be reset when the scope ends */
-#define TEMP_SCOPE ScopedTempBlock const ScopedTempBlock_{ CTX, CTX.temp.GetCursor() }
+#define TEMP_SCOPE ScopedTempBlock const scopedTempBlock_{ CTX, CTX.temp.GetCursor() }
 /** Reset the temporary allocation scope early. All allocations made before this is called will become invalid. */
-#define TEMP_SCOPE_RESET __ScopedTempBlock.Reset()
+#define TEMP_SCOPE_RESET() scopedTempBlock_.Reset()
