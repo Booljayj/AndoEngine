e5fcb581aaaaa4e55fedd05ccf29373fac30ecc4
Justin <booljayj@gmail.com>
2020-02-16 17:45:52 -0800

    Remove old flags macro and fix minor TFlags issues

--- Library/source/Engine/Flags.h
+++ Library/source/Engine/Flags.h
@@ -2,45 +2,11 @@
 #include <type_traits>
 
 /** Test if the bitflag value is set to true in the mask */
-#define TEST_BIT(Mask, flag) ((size_t)(Mask) & (size_t)(flag))
+#define TEST_BIT(Mask, Flag) ((size_t)(Mask) & (size_t)(Flag))
 /** Set the bitflag to true in the mask */
-#define SET_BIT(Mask, flag) ((size_t)(Mask) |= (size_t)(flag))
+#define SET_BIT(Mask, Flag) ((size_t)(Mask) |= (size_t)(Flag))
 /** Set the bitflag to false in the mask */
-#define CLEAR_BIT(Mask, flag) ((size_t)(Mask) &= (size_t)(~(flag)))
-
-/**
- * Defines operators to work with a scoped enum that should act like a set of bitflags
- *     +A : returns A cast to its underlying integer type
- *     !A : returns true if A has no flags (use !! to check if A has any flags)
- *     ~A : returns A with flags flipped
- *     A|B : returns flags that are in A or B
- *     A&B : returns flags that are in both A and B
- *     A^B : returns flags that are different in A and B
- *     A+B : returns flags that are in A or B (alias for |)
- *     A-B : returns flags that are in A without any of the flags in B
- *     A*B : returns true if A contains any of the flags in B
- *     A/B : returns true if A contains all of the flags in B
- */
-#define DEFINE_BITFLAG_OPERATORS(EnumType)\
-constexpr inline std::underlying_type<EnumType>::type operator+(EnumType A) noexcept {\
-	return static_cast<typename std::underlying_type<EnumType>::type>(A);\
-}\
-constexpr inline bool operator!(EnumType A) noexcept { return (+A) == 0; }\
-constexpr inline EnumType operator~(EnumType A) noexcept { return static_cast<EnumType>(~(+A)); }\
-constexpr inline EnumType operator|(EnumType A, EnumType B) noexcept { return static_cast<EnumType>((+A) | (+B)); }\
-constexpr inline EnumType operator&(EnumType A, EnumType B) noexcept { return static_cast<EnumType>((+A) & (+B)); }\
-constexpr inline EnumType operator^(EnumType A, EnumType B) noexcept { return static_cast<EnumType>((+A) ^ (+B)); }\
-constexpr inline EnumType operator+(EnumType A, EnumType B) noexcept { return A|B; }\
-constexpr inline EnumType operator-(EnumType A, EnumType B) noexcept { return static_cast<EnumType>((+A) & (~(+B)));; }\
-constexpr inline EnumType& operator|=(EnumType& A, EnumType B) noexcept { A = A | B; return A; }\
-constexpr inline EnumType& operator&=(EnumType& A, EnumType B) noexcept { A = A & B; return A; }\
-constexpr inline EnumType& operator^=(EnumType& A, EnumType B) noexcept { A = A ^ B; return A; }\
-constexpr inline EnumType& operator+=(EnumType& A, EnumType B) noexcept { A = A + B; return A; }\
-constexpr inline EnumType& operator-=(EnumType& A, EnumType B) noexcept { A = A - B; return A; }\
-[[deprecated("Use operator* instead")]]\
-constexpr inline bool operator<(EnumType A, EnumType B) noexcept { return ((+A) & (+B)) != 0; }\
-constexpr inline bool operator*(EnumType A, EnumType B) noexcept { return ((+A) & (+B)) != 0; }\
-constexpr inline bool operator/(EnumType A, EnumType B) noexcept { return ((+A) & (+B)) == (+B); }\
+#define CLEAR_BIT(Mask, Flag) ((size_t)(Mask) &= (size_t)(~(Flag)))
 
 template<typename EnumType_>
 struct TFlags {
@@ -54,10 +20,8 @@ public:
 	constexpr inline TFlags(const TFlags& other) : flags(other.flags) {}
 	constexpr inline TFlags(UnderlyingType inFlags) : flags(inFlags) {}
 	constexpr inline TFlags(EnumType inFlag) : flags(1 << (UnderlyingType)inFlag) {}
-	constexpr inline TFlags(std::initializer_list<EnumType> const& inFlagList) : TFlags() {
-		for (EnumType flag : inFlagList) {
-			flags |= (1 << (UnderlyingType)flag);
-		}
+	constexpr inline TFlags(std::initializer_list<EnumType> const& inFlags) : TFlags() {
+		for (EnumType flag : inFlags) flags |= (1 << (UnderlyingType)flag);
 	}
 
 	constexpr inline bool operator==(TFlags other) const noexcept { return flags == other.flags; }
@@ -88,7 +52,7 @@ public:
 	constexpr inline bool IsEmpty() const noexcept { return !!flags; }
 
 	/** True if this set of flags is a strict subset of another set of flags */
-	constexpr inline bool IsSubsetOf(TFlags other) const noexcept { return Intersection(Difference(other)).IsEmpty(); }
+	constexpr inline bool IsSubsetOf(TFlags other) const noexcept { return Intersection(other) == flags; }
 
 private:
 	UnderlyingType flags;
