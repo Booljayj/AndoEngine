401953d35d8d9f17c8417df4ae99e0de1a0ac1fd
Justin <booljayj@gmail.com>
2020-09-03 00:19:47 -0700

    Create initial setup for a FlatTree container

---
+++ Library/source/Containers/AllocatorFactories.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <cstddef>
+
+//These classes are allocator adapters, not allocators. They provide an easy way to pass in an allocator type
+//to a template whithout needing the allocated data type, which can be internal to the template. They do this
+//by exposing a "Type" member which is a template that always takes the allocated data type as a parameter.
+
+struct DefaultAllocatorFactory {
+	template<typename DataType>
+	using TAllocator = std::allocator<DataType>;
+};
+
+struct StackAllocatorFactory {
+	template<typename DataType>
+	using TAllocator = TLinearAllocator<DataType>;
+};
+
+/* Not implemented yet
+template<size_t FixedCount>
+struct FixedAllocatorFactory {
+	typename<typename DataType>
+	using TAllocator = FixedAllocatorImpl<DataType, FixedCount>;
+};
+
+template<size_t InlineCount, typename FallbackAllocator>
+struct InlineAllocatorFactory {
+	template<typename DataType>
+	using TAllocator = InlineAllocatorImpl<DataType, InlineAllocator, FallbackAllocator::Type<DataType>>;
+};
+*/
---
+++ Library/source/Containers/FlatTree.h
@@ -0,0 +1,81 @@
+#pragma once
+#include <cstdint>
+#include <vector>
+#include "Containers/AllocatorFactories.h"
+
+/**
+ * A tree structure that is stored in a linear array, using indices to define the tree.
+ * Ideal for fast-iterating trees where the nodes are not moved after creation.
+ */
+template<typename NodeDataType, typename AllocatorFactory = DefaultAllocatorFactory>
+struct FlatTree {
+public:
+	using IndexType = uint32_t;
+	static constexpr IndexType None = static_cast<IndexType>(-1);
+
+	struct Node {
+		using DataType = NodeDataType;
+
+		IndexType parent;
+    	IndexType firstChild;
+    	IndexType lastChild;
+    	IndexType nextSibling;
+    	IndexType prevSibling;
+
+		DataType data;
+
+		inline bool IsRoot() const { return parent == None; }
+		inline bool IsLeaf() const { return firstChild == None; }
+	};
+
+	using Allocator = typename AllocatorFactory::template TAllocator<Node>;
+
+	struct NodeHandle {
+	private:
+		FlatTree* tree = nullptr;
+		IndexType index = None;
+
+		NodeHandle(FlatTree* inTree, IndexType inIndex)
+		: tree(inTree), index(inIndex)
+		{}
+
+	public:
+		NodeHandle() = default;
+		NodeHandle(const NodeHandle& other) = default;
+
+		static const NodeHandle Invalid = NodeHandle{ nullptr, None };
+
+		operator bool() const { return tree && tree->IsValidIndex(index); }
+
+		typename Node::DataType const* Get() const { return bool(*this) ? &tree->nodes[index] : nullptr; }
+		typename Node::DataType* Get() { return const_cast<typename Node::DataType*>(static_cast<const NodeHandle*>(this)->Get()); }
+
+		typename Node::DataType const* operator->() const { return tree->nodes[index]; }
+		typename Node::DataType* operator->() { return tree->nodes[index]; }
+
+		NodeHandle Parent() const { return bool(*this) ? NodeHandle{tree, tree->nodes[index].parent} : NodeHandle::Invalid; }
+		NodeHandle FirstChild() const { return bool(*this) ? NodeHandle{tree, tree->nodes[index].firstChild} : NodeHandle::Invalid; }
+		NodeHandle LastChild() const { return bool(*this) ? NodeHandle{tree, tree->nodes[index].lastChild} : NodeHandle::Invalid; }
+		NodeHandle NextSibling() const { return bool(*this) ? NodeHandle{tree, tree->nodes[index].nextSibling} : NodeHandle::Invalid; }
+		NodeHandle PrevSibling() const { return bool(*this) ? NodeHandle{tree, tree->nodes[index].prevSibling} : NodeHandle::Invalid; }
+	};
+
+	FlatTree() = default;
+	FlatTree(Allocator const& allocator)
+	: nodes(allocator)
+	{}
+
+	inline IndexType Size() const { return nodeCount; }
+	inline IndexType Capacity() const { return nodes.size(); }
+	inline IndexType Slack() const { return nodes.size() - nodeCount; }
+
+	void Reserve(IndexType size) {
+		nodes.reserve(size);
+	}
+
+private:
+	std::vector<Node, Allocator> nodes;
+	IndexType nodeCount = 0;
+
+	bool IsValidIndex(IndexType index) { return index < nodes.size(); }
+};
