4a731dee0e3c0d94e08f1097172fe9743ef1b88e
Justin <booljayj@gmail.com>
2020-12-13 19:37:05 -0800

    Add reflection for the Color struct

---
+++ Library/source/Rendering/Color.cpp
@@ -0,0 +1,18 @@
+#include "Rendering/Color.h"
+#include "Reflection/StandardResolvers.h"
+#include "Reflection/StructTypeInfo.h"
+
+namespace Reflection {
+	const TStructTypeInfo<Rendering::Color> info_Color = TStructTypeInfo<Rendering::Color>{}
+		.Description("4-component color"sv)
+		.Variables({
+			VariableInfo{ &Rendering::Color::r, "r"sv, nullptr, FVariableFlags::None },
+			VariableInfo{ &Rendering::Color::g, "g"sv, nullptr, FVariableFlags::None },
+			VariableInfo{ &Rendering::Color::b, "b"sv, nullptr, FVariableFlags::None },
+			VariableInfo{ &Rendering::Color::a, "a"sv, nullptr, FVariableFlags::None }
+		});
+
+	namespace Internal {
+		TypeInfo const* TypeResolver_Implementation<Rendering::Color>::Get() { return &info_Color; }
+	}
+}
--- Library/source/Rendering/Color.h
+++ Library/source/Rendering/Color.h
@@ -1,6 +1,13 @@
 #pragma once
 #include "Geometry/GLM.h"
+#include "Reflection/TypeResolver.h"
 
 namespace Rendering {
 	using Color = glm::vec<4, uint8_t>;
+}
+
+namespace Reflection {
+	namespace Internal {
+		DECLARE_RESOLVER(Rendering::Color);
+	}
 }
