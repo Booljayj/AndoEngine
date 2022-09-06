c4500c3cc1c22eef89598a2d5047c6eda6a7e262
Justin <booljayj@gmail.com>
2022-09-05 17:43:05 -0700

    Improve RemoveSwap implementations

--- Library/source/Engine/Algo.h
+++ Library/source/Engine/Algo.h
@@ -1,27 +1,37 @@
 #include "Engine/StandardTypes.h"
 
 namespace Algo {
-	/** Remove an element from the array by swapping it with the last element and removing the last. Faster removal but does not preserve order. */
-	template<typename TRange, typename TRangeValue>
-	bool RemoveSwap(TRange& range, TRangeValue const& value) {
-		auto const iter = std::find(range.begin(), range.end(), value);
-		if (iter != range.end()) {
-			std::iter_swap(iter, range.end() - 1);
+	/** Remove an element from a container by swapping it with the last element and removing the last. Faster removal but does not preserve order. */
+	template<typename TRange>
+	bool RemoveSwap(TRange& range, size_t index) {
+		if (index < range.size()) {
+			if (index < (range.size() - 1)) std::swap(range[index], range.back());
 			range.pop_back();
 			return true;
 		}
-		else return false;
+		return false;
 	}
 
-	/** Remove an element from the array by swapping it with the last element and removing the last. Faster removal but does not preserve order. */
-	template<typename TRange, typename TPredicate>
-	bool RemoveSwapIf(TRange& range, TPredicate&& predicate) {
-		auto const iter = std::find_if(range.begin(), range.end(), std::forward<TPredicate>(predicate));
+	/** Remove an element from a container by swapping it with the last element and removing the last. Faster removal but does not preserve order. */
+	template<typename TRange>
+	bool RemoveSwap(TRange& range, typename TRange::iterator const& iter) {
 		if (iter != range.end()) {
-			std::iter_swap(iter, range.end() - 1);
+			if (std::addressof(*iter) != std::addressof(range.back())) std::swap(*iter, range.back());
 			range.pop_back();
 			return true;
 		}
-		else return false;
+		return false;
+	}
+
+	/** Remove an element from a container by swapping it with the last element and removing the last. Faster removal but does not preserve order. */
+	template<typename TRange>
+	bool RemoveSwap(TRange& range, typename TRange::value_type const& value) {
+		return RemoveSwap(range, std::find(range.begin(), range.end(), value));
+	}
+
+	/** Remove an element from  a container by swapping it with the last element and removing the last. Faster removal but does not preserve order. */
+	template<typename TRange, typename TPredicate>
+	bool RemoveSwapIf(TRange& range, TPredicate&& predicate) {
+		return RemoveSwap(range, std::find_if(range.begin(), range.end(), std::forward<TPredicate>(predicate)));
 	}
 }
